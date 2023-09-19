c93f6fbf6fc4cbbe3b658cc4049c96aed6439a69
vbea <vbea@foxmail.com>
2023-09-19 15:39:25 +0800

    add

--- src/components/TableAction.vue
+++ src/components/TableAction.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="table-actions">
+  <div class="vbe-table-actions">
     <div class="table-action action-edit" @click="handleView" v-if="showView">
-      <i v-if="showIcon" class="el-icon el-icon-view"></i>
+      <i v-if="showIcon" class="el-icon-view"></i>
       <span>{{viewText}}</span>
     </div>
     <div class="table-action action-edit" @click="handleEdit" v-if="showEdit">
-      <img v-if="showIcon" src="../assets/ic-update.png"/>
+      <i v-if="showIcon" class="el-icon-edit-outline"/>
       <span>{{editText}}</span>
     </div>
     <div class="table-action action-delete" @click="handleDelete" v-if="showDel">
-      <img v-if="showIcon" src="../assets/ic-delete.png" />
+      <i v-if="showIcon" class="el-icon-delete"/>
       <span>{{deleteText}}</span>
     </div>
   </div>
@@ -63,45 +63,57 @@
 </script>
 
 <style lang="scss">
-  .table-actions {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.vbe-table-actions {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
   .table-action {
-    padding: 0 14px;
+    padding: 0 8px;
     font-size: 14px;
     cursor: pointer;
     display: flex;
     align-items: center;
     position: relative;
-    & span {
+    i {
+      font-size: 15px;
+      margin-right: 3px;
+    }
+    span {
       white-space: nowrap;
     }
-    & + .table-action::before {
+    & + .table-action::after {
       left: 0;
       height: 14px;
       content: " ";
       position: absolute;
       border-left: 1px solid #999;
     }
+    &::before {
+      top: auto;
+      left: 6px;
+      right: 5px;
+      bottom: 1px;
+      height: 1px;
+      content: " ";
+      position: absolute;
+      transform: scaleX(0);
+      transition: all .2s;
+      background-color: #3179E4;
+      backface-visibility: hidden;
+    }
+    &:hover::before {
+      transform: scaleX(1);
+    }
   }
   .action-edit {
     color: #3179E4;
-    img {
-      width: 15px;
-      margin-right: 5px;
-    }
-    .el-icon {
-      font-size: 16px;
-      font-weight: bold;
-      margin-right: 5px;
-    }
   }
   .action-delete {
     color: #ED3F3F;
-    img {
-      width: 20px;
+    &::before {
+      background-color: #ED3F3F;
     }
   }
+}
 </style>
